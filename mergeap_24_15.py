93defd8737cb376f9fff4713a3846538d8308ee9
RATHASATE KUNSUEK <konicabook@gmail.com>
2026-03-04 23:05:41 +0700

    Update mergeap_24_15.py

--- mergeap_24_15.py
+++ mergeap_24_15.py
@@ -87,8 +87,39 @@ choices = ["PDA", "GOT", "PDA_AUDIT"]
 
 result["Device_type"] = np.select(conditions, choices, default="Unknown")
 
-#remove column index 8-15
+#remove column index 1 and 8
 result = result.drop(result.columns[[1,8]], axis=1)
+
+#rename column "Last Association Time" to "Last_Association_Time"
+result = result.rename(columns={"Last Association Time": "Last_Association_Time"})
+
+#rename column "MAC Address" to "MAC_Address"
+result = result.rename(columns={"MAC Address": "MAC_Address"})
+
+#rename column "Vendor" to "Vendor_Name"
+result = result.rename(columns={"Vendor": "Vendor_Name"})
+
+#rename column "IP Address" to "IP_Address"
+result = result.rename(columns={"IP Address": "IP_Address"})
+
+#rename column "AP Name" to "AP_Name"
+result = result.rename(columns={"AP Name": "AP_Name"})
+
+#rename column "802.11 State" to "State_802.11"
+result = result.rename(columns={"802.11 State": "Device_State"})
+
+#rename column "SSID" to "SSID_Name"
+result = result.rename(columns={"SSID": "SSID_Name"})
+
+#replace value in column "Device_State" if value is "Disassociated" to "Disconnected" else value is "Associated" to "Connected" 
+result["Device_State"] = result["Device_State"].replace({
+    "Disassociated": "Disconnected",
+    "Associated": "Connected"
+})
+
+#insert new column after column "Last_Association_Time" with name "Disconnected_Time" and value with calculation of current time - last association time if device state is "Disconnected" else value is empty from Column "Last_Association_Time and Today date time"
+from datetime import datetime
+
 
 # Save the result to a new CSV file
 result.to_csv(r'D:\ITH\tempdownload\result.csv', index=False)
